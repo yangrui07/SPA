596d4371d0c083eebe34af6a065e9c329acf4675
Rui Yang <ruiy@google.com>
2018-12-24 16:05:13 +0800

    add jpg

--- SPA/js/custom.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-	$(window).scroll(function(){
-		if ($(window).scrollTop() < 55) {
-			$('.home .navbar-fixed-top').removeClass('home-navbar-fixed');
-		}else{
-			$('.home .navbar-fixed-top').addClass('home-navbar-fixed');
-		}
-	});
-});
